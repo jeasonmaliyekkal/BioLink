a5d7ebbc9a362345b8351f78c35c17da440d5cf6
Jeason Paul <jeasonpaulmaliyekkal@gmail.com>
2024-12-01 13:00:42 +0000

    Added files, build not working

---
+++ Boot/tmp/BioLink_Boot-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/home/jeason/Projects/stm32/BioLink/Boot"
+  "/home/jeason/Projects/stm32/BioLink/Boot/build"
+  "/home/jeason/Projects/stm32/BioLink/Boot"
+  "/home/jeason/Projects/stm32/BioLink/Boot/tmp"
+  "/home/jeason/Projects/stm32/BioLink/Boot/src/BioLink_Boot-stamp"
+  "/home/jeason/Projects/stm32/BioLink/Boot/src"
+  "/home/jeason/Projects/stm32/BioLink/Boot/src/BioLink_Boot-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/home/jeason/Projects/stm32/BioLink/Boot/src/BioLink_Boot-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/home/jeason/Projects/stm32/BioLink/Boot/src/BioLink_Boot-stamp${cfgdir}") # cfgdir has leading slash
+endif()
---
+++ build/Debug/Appli/tmp/BioLink_Appli-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/home/jeason/Projects/stm32/BioLink/Appli"
+  "/home/jeason/Projects/stm32/BioLink/Appli/build"
+  "/home/jeason/Projects/stm32/BioLink/build/Debug/Appli"
+  "/home/jeason/Projects/stm32/BioLink/build/Debug/Appli/tmp"
+  "/home/jeason/Projects/stm32/BioLink/build/Debug/Appli/src/BioLink_Appli-stamp"
+  "/home/jeason/Projects/stm32/BioLink/build/Debug/Appli/src"
+  "/home/jeason/Projects/stm32/BioLink/build/Debug/Appli/src/BioLink_Appli-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/home/jeason/Projects/stm32/BioLink/build/Debug/Appli/src/BioLink_Appli-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/home/jeason/Projects/stm32/BioLink/build/Debug/Appli/src/BioLink_Appli-stamp${cfgdir}") # cfgdir has leading slash
+endif()
